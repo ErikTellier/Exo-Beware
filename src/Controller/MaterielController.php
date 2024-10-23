487f62e8bb243ef50a9765c5dfef3f9e76646bd0
ErikTellier <erik.tellier.etps@gmail.com>
2024-10-23 03:24:19 +0200

    Added popup and decremente

--- src/Controller/MaterielController.php
+++ src/Controller/MaterielController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Materiel;
+use App\Entity\Tva;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,7 +19,7 @@ class MaterielController extends AbstractController
         return $this->render('materiel/index.html.twig');
     }
 
-    #[Route('/materiel/data', name: 'materiel_data', methods: ['GET'])]
+    #[Route('/data', name: 'materiel_data', methods: ['GET'])]
     public function data(Request $request, EntityManagerInterface $em): JsonResponse
     {
         $start = $request->query->getInt('start', 0);
@@ -32,7 +33,7 @@ class MaterielController extends AbstractController
     
         // Fetch paginated results
         $materiels = $queryBuilder
-            ->select('m')  // Ensure we are selecting the full entity
+            ->select('m')
             ->setFirstResult($start)
             ->setMaxResults($length)
             ->getQuery()
@@ -41,8 +42,16 @@ class MaterielController extends AbstractController
         // Prepare the data for the DataTable
         $data = [];
         foreach ($materiels as $materiel) {
+            $tva = $materiel->getTva();
             $data[] = [
+                'id' => $materiel->getId(),
                 'nom' => $materiel->getNom(),
+                'prixHT' => $materiel->getPrixHT(),
+                'prixTTC' => $materiel->getPrixTTC(),
+                'tvaLibelle' => $tva ? $tva->getLibelle() : 'N/A',  // Display TVA libelle
+                'tvaValeur' => $tva ? $tva->getValeur() : 'N/A',    // Display TVA valeur
+                'quantite' => $materiel->getQuantite(),
+                'creationDate' => $materiel->getCreationDate()->format('Y-m-d H:i:s'),  // Format date
             ];
         }
     
@@ -50,9 +59,36 @@ class MaterielController extends AbstractController
         return new JsonResponse([
             'draw' => $request->query->getInt('draw'),
             'recordsTotal' => $totalRecords,
-            'recordsFiltered' => $totalRecords,  // Since there is no filtering, use total records
+            'recordsFiltered' => $totalRecords,
             'data' => $data,
         ]);
+    }
+
+    #[Route('/decrement/{id}', name: 'decrement_materiel', methods: ['POST'])]
+    public function decrementMateriel(int $id, EntityManagerInterface $entityManager): JsonResponse
+    {
+        // Récupérer le produit via le repository
+        $materiel = $entityManager->getRepository(Materiel::class)->find($id);
+
+        if (!$materiel) {
+            return new JsonResponse(['status' => 'Produit non trouvé'], 404);
+        }
+
+        // Décrémenter le stock
+        $currentQuantity = $materiel->getQuantite();
+        if ($currentQuantity > 0) {
+            $materiel->setQuantite($currentQuantity - 1);
+
+            // Si le stock est à 0, supprimer le produit
+            if ($materiel->getQuantite() == 0) {
+                $entityManager->remove($materiel);
+            }
+
+            // Sauvegarder les changements
+            $entityManager->flush();
+        }
+
+        return new JsonResponse(['status' => 'Quantité décrémentée avec succès']);
     }
     
 }
